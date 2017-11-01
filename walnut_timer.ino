1ab85d7e86f8d00f89184ccc91981a0767c62cf7
Jon Lamb <jlamb@polysync.io>
2017-11-01 06:06:02 -0700

    Update debouncing

--- walnut_timer.ino
+++ walnut_timer.ino
@@ -1,17 +1,21 @@
 // TODO
 // - cleanup pitches.h
+// - use timer1 for time counter
+// could use a library, but easier to just setup the registers
+// http://www.robotshop.com/letsmakerobots/arduino-101-timers-and-interrupts
 
 #include <stdint.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_LEDBackpack.h>
-
+#include <Bounce2.h>
 #include "pitches.h"
 
 #define DISPLAY_ADDRESS (0x70)
 #define BRIGHTNESS (15)
 
 #define AMP_ENABLE_DELAY (5)
+#define DEBOUNCE_DELAY (5)
 
 #define PIN_AMP_SHUTDOWN (2)
 #define PIN_AUDIO (3)
@@ -31,6 +35,9 @@ typedef struct
 } melody_tone_s;
 
 Adafruit_7segment seg_display = Adafruit_7segment();
+Bounce sw0_db = Bounce();
+Bounce sw1_db = Bounce();
+Bounce sw2_db = Bounce();
 
 static const melody_tone_s melody_tones[] =
 {
@@ -68,7 +75,7 @@ static void play_melody(void)
     {
         tone(PIN_AUDIO, melody_tones[idx].freq, melody_tones[idx].duration);
 
-        // plus 30 % of duration
+        // plus 30 % of duration between notes
         const float delay_f = ((float) melody_tones[idx].duration) * 1.30f;
         delay((unsigned long) delay_f);
 
@@ -89,6 +96,13 @@ void setup()
     pinMode(PIN_SW0, INPUT_PULLUP);
     pinMode(PIN_SW1, INPUT_PULLUP);
     pinMode(PIN_SW2, INPUT_PULLUP);
+
+    sw0_db.attach(PIN_SW0);
+    sw0_db.interval(DEBOUNCE_DELAY);
+    sw1_db.attach(PIN_SW1);
+    sw1_db.interval(DEBOUNCE_DELAY);
+    sw2_db.attach(PIN_SW2);
+    sw2_db.interval(DEBOUNCE_DELAY);
     
     seg_display.begin(DISPLAY_ADDRESS);
     seg_display.setBrightness(BRIGHTNESS);
@@ -97,6 +111,16 @@ void setup()
 void loop()
 {
     //seg_display.writeDisplay();
+
+    // returns true on state change
+    sw0_db.update();
+    sw1_db.update();
+    sw2_db.update();
+
+    // time +/- should show/adjust the time until timer is started
+    // when timer is running, time counts up, pause on press, resume on press, reset on hold?
+    
+    // sw0_db.read()
 
     play_melody();
     

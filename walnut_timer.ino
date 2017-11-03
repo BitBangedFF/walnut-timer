d46a3acf2f72f5eefeaf781f3cdac75152426fb4
Jon Lamb <jlamb@polysync.io>
2017-11-03 14:32:52 -0700

    Add short press detection

--- walnut_timer.ino
+++ walnut_timer.ino
@@ -24,10 +24,10 @@
 #define AMP_ENABLE_DELAY (5UL)
 #define RENDER_DELAY (10UL)
 
-#define BUTTON_HOLD_INTERVAL (400)
+#define BUTTON_HOLD_INTERVAL (200UL)
 #define DEBOUNCE_INTERVAL (5UL)
 
-#define DEFAULT_TIMER_SETPOINT_SEC (10UL)
+#define DEFAULT_TIMER_SETPOINT_SEC (30UL)
 
 #define PIN_AMP_SHUTDOWN (2)
 #define PIN_AUDIO (3)
@@ -52,7 +52,8 @@ Adafruit_7segment seg_display = Adafruit_7segment();
 Bounce sw0_db = Bounce();
 Bounce sw1_db = Bounce();
 Bounce sw2_db = Bounce();
-elapsedMillis timer_elapsed_ms;
+elapsedMillis timer_elapsed;
+elapsedMillis adj_hold_elapsed;
 
 static unsigned long timer_setpoint;
 static unsigned long timer_paused_value;
@@ -108,23 +109,52 @@ static void play_melody(void)
 static bool check_for_timer_adjustment(void)
 {
     bool is_adj = false;
+    unsigned long adj = 0;
 
-    if((sw2_db.read() == false) && (sw2_db.fell() == true))
+    if(sw2_db.read() == false)
     {
         is_adj = true;
 
-        if(timer_setpoint < (ULONG_MAX - 1))
+        if(sw2_db.fell() == true)
         {
-            timer_setpoint += SEC_TO_MS(1);
+            adj_hold_elapsed = 0;
+            adj = SEC_TO_MS(1);
+        }
+        else if(adj_hold_elapsed > BUTTON_HOLD_INTERVAL)
+        {
+            adj_hold_elapsed = 0;
+            adj = SEC_TO_MS(2);
+        }
+
+        if(adj != 0)
+        {
+            if(timer_setpoint < (ULONG_MAX - 1))
+            {
+                timer_setpoint += adj;
+            }
         }
     }
-    else if((sw1_db.read() == false) && (sw1_db.fell() == true))
+    else if(sw1_db.read() == false)
     {
         is_adj = true;
 
-        if(timer_setpoint > SEC_TO_MS(1))
+        if(sw1_db.fell() == true)
         {
-            timer_setpoint -= SEC_TO_MS(1);
+            adj_hold_elapsed = 0;
+            adj = SEC_TO_MS(1);
+        }
+        else if(adj_hold_elapsed > BUTTON_HOLD_INTERVAL)
+        {
+            adj_hold_elapsed = 0;
+            adj = SEC_TO_MS(2);
+        }
+
+        if(adj != 0)
+        {
+            if(timer_setpoint > SEC_TO_MS(1))
+            {
+                timer_setpoint -= adj;
+            }
         }
     }
 
@@ -134,20 +164,20 @@ static bool check_for_timer_adjustment(void)
 static bool check_for_start_stop(void)
 {
     bool changed = false;
-    
+
     if((sw0_db.read() == false) && (sw0_db.fell() == true))
     {
         changed = true;
 
         if(timer_running == true)
         {
-            timer_paused_value = timer_elapsed_ms;
+            timer_paused_value = timer_elapsed;
             timer_running = false;
         }
         else
         {
             timer_running = true;
-            timer_elapsed_ms = timer_paused_value;
+            timer_elapsed = timer_paused_value;
             timer_paused_value = 0;
         }
     }
@@ -171,9 +201,9 @@ static void render_timer_elapsed(void)
 
     if(timer_running == true)
     {
-        if(timer_elapsed_ms <= timer_setpoint)
+        if(timer_elapsed <= timer_setpoint)
         {
-            ms_val = (timer_setpoint - timer_elapsed_ms);
+            ms_val = (timer_setpoint - timer_elapsed);
         }
         else
         {
@@ -220,7 +250,7 @@ void setup()
     sw2_db.interval(DEBOUNCE_INTERVAL);
 
     timer_setpoint = SEC_TO_MS(DEFAULT_TIMER_SETPOINT_SEC);
-    timer_elapsed_ms = 0;
+    timer_elapsed = 0;
     timer_paused_value = 0;
     timer_running = false;
     is_adjusting = true;
@@ -249,7 +279,7 @@ void loop()
         is_adjusting = true;
         timer_paused_value = 0;
     }
-    
+
     if(check_for_start_stop() == true)
     {
         is_adjusting = false;
@@ -257,8 +287,8 @@ void loop()
 
     if(timer_running == true)
     {
-        if(timer_elapsed_ms >= timer_setpoint)
-        {   
+        if(timer_elapsed >= timer_setpoint)
+        {
             seg_display.blinkRate(1);
             timer_running = false;
             is_adjusting = true;
